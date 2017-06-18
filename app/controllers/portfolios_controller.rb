62aae96dee9f6c8fb660490d603661784e381a98
Arpit Agarwal <agarwalarpit22@gmail.com>
2017-06-18 21:24:49 +0000

    Integrated portfolios new and create method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,6 +8,24 @@ class PortfoliosController < ApplicationController
       @portfolio_item = Portfolio.new
     end
     
+    def create
+      @portfolio_item = Portfolio.new(portfolio_params)
+  
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'New Portfolio Item was successfully added.' }
+        else
+          format.html { render :new }
+        end
+      end
+    end
+    
     def show
     end
+    
+    
+    private
+      def portfolio_params
+        params.require(:portfolio).permit(:title, :subtitle, :body)
+      end
 end
