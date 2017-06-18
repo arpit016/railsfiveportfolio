261ab5f046eed68f515926dcd55af0dd48e7c523
Arpit Agarwal <agarwalarpit22@gmail.com>
2017-06-18 22:07:04 +0000

    Integrated edit update portfolio functions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
     
     def index
       @portfolio_items = Portfolio.all
@@ -20,11 +21,35 @@ class PortfoliosController < ApplicationController
       end
     end
     
+    def edit
+    end
+    
+    def update
+      respond_to do |format|
+        if @portfolio_item.update(portfolio_params)
+          format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully updated.'}
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+    
+    def destroy
+      @portfolio_item.destroy
+      respond_to do |format|
+        format.html { redirect_to portfolios_path, notice: 'Portfolio Item was successfully deleted.'}
+      end
+    end
+    
     def show
     end
     
     
     private
+      def set_portfolio
+        @portfolio_item = Portfolio.find(params[:id])
+      end
+      
       def portfolio_params
         params.require(:portfolio).permit(:title, :subtitle, :body)
       end
