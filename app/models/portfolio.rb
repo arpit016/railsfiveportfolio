79f05ea66d8dc362aa4229171c14060f74c126f7
Arpit Agarwal <agarwalarpit22@gmail.com>
2017-06-30 10:38:07 +0000

    Image uploading to s3 completed for both blogs and portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,11 +8,4 @@ class Portfolio < ApplicationRecord
     
     mount_uploader :thumb_image, PortfolioUploader
     mount_uploader :main_image, PortfolioUploader
-    
-    after_initialize :set_defaults
-    
-    def set_defaults
-       self.main_image ||= "http://via.placeholder.com/600x400"
-       self.thumb_image ||= "http://via.placeholder.com/350x200"
-    end
 end
