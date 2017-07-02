c67b53e6e0eecc6c37096c4d5c1b7e291fa202ec
Arpit Agarwal <agarwalarpit22@gmail.com>
2017-07-02 06:48:31 +0000

    Added ajax to delete and toggle status action

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '*path' => redirect('/')
