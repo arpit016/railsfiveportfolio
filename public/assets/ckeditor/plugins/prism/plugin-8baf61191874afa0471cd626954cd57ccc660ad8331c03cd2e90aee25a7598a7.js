edb29c261b626927638cc994314e91253ad579de
Arpit Agarwal <agarwalarpit22@gmail.com>
2017-07-08 22:29:06 +0000

    Parsing error

---
+++ public/assets/ckeditor/plugins/prism/plugin-8baf61191874afa0471cd626954cd57ccc660ad8331c03cd2e90aee25a7598a7.js
@@ -0,0 +1,106 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+/**
+ * @fileOverview Rich code snippets for CKEditor.
+ */
+
+
+'use strict';
+
+(function() {
+  // Create a new plugin which registers a custom code highlighter
+  // based on Prism.js in order to replace the one that comes
+  // with the Code Snippet plugin.
+  CKEDITOR.plugins.add('prism', {
+    requires: 'codesnippet',
+
+    init: function(editor) {
+      var path = this.path;
+
+      // Loading the prism.js style file.
+      // Idea taken from codesnippet/plugin.js code.
+      // Method is available only if wysiwygarea exists and
+      // CKEditor is at least version 4.4.
+      if (editor.addContentsCss) {
+        //editor.addContentsCss(path + 'lib/prism/prism_patched.min.css');
+        editor.addContentsCss(path + 'lib/prism/prism.css');
+      }
+
+      // Create a new instance of the highlighter.
+      var prismHighlighter = new CKEDITOR.plugins.codesnippet.highlighter({
+        init: function(ready) {
+          // Load the Prism.js library asynchronously.
+          //CKEDITOR.scriptLoader.load(path + 'lib/prism/prism_patched.min.js', function() {
+          CKEDITOR.scriptLoader.load(path + 'lib/prism/prism.js', function() {
+            // Notify the handler that the library has been loaded.
+            ready();
+          });
+        },
+
+        // Specify the supported languages.
+        languages: {
+          apacheconf: 'Apache Conf',
+          bash: 'Bash',
+          coffeescript: 'CoffeeScript',
+          css: 'CSS',
+          git: 'Git',
+          haml: 'Haml',
+          markup: 'HTML',
+          http: 'HTTP',
+          ini: 'INI',
+          java: 'Java',
+          javascript: 'JavaScript',
+          less: 'Less',
+          markdown: 'Markdown',
+          nginx: 'Nginx',
+          python: 'Python',
+          ruby: 'Ruby',
+          sas: 'SAS',
+          sass: 'Sass',
+          scss: 'SCSS',
+          sql: 'SQL',
+          swift: 'Swift',
+          typescript: 'Typescript',
+          vim: 'vim',
+          yaml: 'YAML',
+        },
+
+        highlighter: function(code, language, callback) {
+          // _self.Prism is a global namespace/object created by Prism.js.
+          var _prism = _self.Prism;
+
+          // Let the Prism.js highlight the code.
+          var highlightedCode = _prism.highlight(code, _prism.languages[language], language);
+
+          // The clever idea below is taken from the 'Line Numbers' plugin
+          // of Prism. Basically, we want to count the number of newlines (\n)
+          // in the highlighted code, then create the same number
+          // of SPAN elements, append them to the highlighted code
+          // and finally number/label them using prism.css.
+          var match = highlightedCode.match(/\n(?!$)/g);
+          var linesNum = match ? match.length + 1 : 1;
+
+          var lines = new Array(linesNum + 1);
+          lines = lines.join('<span></span>');
+
+          // Create the SPAN root/wrapper, insert its child SPAN lines,
+          // then append them to the highlighted code.
+          var lineNumbersWrapper = '<span class="line-numbers-rows">';
+          lineNumbersWrapper += lines;
+          lineNumbersWrapper += '</span>';
+          highlightedCode += lineNumbersWrapper;
+
+          // Return highlighted code.
+          callback(highlightedCode);
+        }
+      });
+
+      // From now on, prismHighlighter will be used as a Code Snippet
+      // highlighter, overwriting the default engine.
+      editor.plugins.codesnippet.setHighlighter(prismHighlighter );
+    }
+  });
+})();
